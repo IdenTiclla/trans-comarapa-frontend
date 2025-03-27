f051af3a7ce5e6907472f9da61182f1aa643b5e5
Iden Ticlla <iden.ticlla@gmail.com>
2025-03-27 16:36:26 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello world trans-comarapa</h1>
+    <button @click="handleclickfunction">press meee</button>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const handleclickfunction = () => {
+  console.log("hello world")
+}
+</script>
